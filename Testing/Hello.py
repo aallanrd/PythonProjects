148e7e18bcb65fd3c4d94e4ce1cce4f82b0d121b
aallanrd <allanjose91@gmail.com>
2015-04-20 12:30:19 -0500

    Python Easy
    Easy Starting With Python

---
+++ Testing/Hello.py
@@ -0,0 +1,32 @@
+#This file it's located at
+#C:/Users/Allan/Documents/PythonProjects/Testing/
+#Allan Practice Python
+
+#Simpliest Hello World Print
+print ("Hello World")
+myName = input("What's your name?\n")
+print(myName)
+
+count = 0
+#Performing Conditions 
+if(myName == "Allan"):
+       print ('Welcome Guest' , myName)  
+else:
+   print ('Not Welcome Guest')  
+
+   
+#Definig Functions
+
+   
+def greet(name):
+    print 'Hello', name
+greet('Jack')
+greet('Jill')
+greet('Bob')
+
+  
+   
+
+
+
+
